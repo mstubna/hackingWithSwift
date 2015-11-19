dc3a184a72b5f18110cce628fd4796d79e85a498
Mike Stubna <mike@stubna.com>
2015-11-19 11:09:40 -0500

    project 2 initial checkin

---
+++ 2/Project2/Project2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Mike Stubna on 11/19/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
