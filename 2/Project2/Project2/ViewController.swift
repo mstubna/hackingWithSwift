573506d32161faf92bfac4672d6d4523c05cf591
Mike Stubna <mike@stubna.com>
2015-11-19 11:56:02 -0500

    styling buttons

--- 2/Project2/Project2/ViewController.swift
+++ 2/Project2/Project2/ViewController.swift
@@ -14,11 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        styleButton(button1)
+        styleButton(button2)
+        styleButton(button3)
+        
+        askQuestion()
+    }
+    
+    func styleButton(button: UIButton) {
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGrayColor().CGColor
     }
 
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
