21e5e9d05815cd1cac1876f1f532599c5b813597
Mike Stubna <mike@stubna.com>
2015-11-27 09:54:49 -0500

    adding words source file and initialization logic

--- 5/Project5/Project5/MasterViewController.swift
+++ 5/Project5/Project5/MasterViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import GameKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var objects = [String]()
+    var allWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
+    }
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
@@ -39,8 +58,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
