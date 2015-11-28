5be2cb69cff79f3a7406dac22f5ca3f8bd059352
Mike Stubna <mike@stubna.com>
2015-11-28 09:32:23 -0500

    copying Project2 -> Project6a

---
+++ 6/Project6a/Project6a/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Mike Stubna on 11/19/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        styleButton(button1)
+        styleButton(button2)
+        styleButton(button3)
+
+        askQuestion()
+    }
+    
+    func styleButton(button: UIButton) {
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGrayColor().CGColor
+    }
+
+    func askQuestion() {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        
+        title = countries[correctAnswer].uppercaseString
+    }
+
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+        var color: UIColor
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            color = UIColor.greenColor()
+            ++score
+        } else {
+            title = "Incorrect"
+            color = UIColor.redColor()
+            --score
+        }
+        
+        label.text = "\(title)! Your score is \(score)."
+        label.textColor = color
+
+        askQuestion()
+    }
+    
+    func continueHandler(action: UIAlertAction) {
+        askQuestion()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
