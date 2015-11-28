1351651a45483e7b4c0bbcbb8a8269e625cbf111
Mike Stubna <mike@stubna.com>
2015-11-28 15:48:13 -0500

    fleshing out constraints

--- 6/Project6b/Project6b/ViewController.swift
+++ 6/Project6b/Project6b/ViewController.swift
@@ -50,7 +50,9 @@ class ViewController: UIViewController {
             view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
         
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        let metrics = ["labelHeight": 88]
+        
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-==10-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
 
     }
     
