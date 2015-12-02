5e0285527f4f86eb1fb2b1b2cfdb9a0da4d9466d
Mike Stubna <mike@stubna.com>
2015-12-02 10:56:37 -0500

    parsing json data from WH api

--- 7/Project7/Project7/MasterViewController.swift
+++ 7/Project7/Project7/MasterViewController.swift
@@ -11,11 +11,35 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [[String: String]]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    parseJSON(json)
+                }
+            }
+        }
+    }
+    
+    func parseJSON(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        
+        tableView.reloadData()
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -55,8 +79,9 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
         return cell
     }
 
