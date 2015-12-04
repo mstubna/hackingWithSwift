b0df5880b5651dc69dcdf6b880cfa30aaf9fe9da
Mike Stubna <mike@stubna.com>
2015-12-04 10:34:20 -0500

    more game setup

--- 8/Project8/Project8/ViewController.swift
+++ 8/Project8/Project8/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
@@ -19,9 +20,65 @@ class ViewController: UIViewController {
     @IBAction func clearTapped(sender: UIButton) {
     }
     
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLetterButtons()
+        loadLevel()
+    }
+    
+    func setupLetterButtons() {
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: "letterTapped:", forControlEvents: .TouchUpInside)
+        }
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = NSBundle.mainBundle().pathForResource("level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath, usedEncoding: nil) {
+                var lines = levelContents.componentsSeparatedByString("\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(lines) as! [String]
+                
+                for (index, line) in lines.enumerate() {
+                    let parts = line.componentsSeparatedByString(": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.stringByReplacingOccurrencesOfString("|", withString: "")
+                    solutionString += "\(solutionWord.characters.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.componentsSeparatedByString("|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        cluesLabel.text = clueString.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet())
+        answersLabel.text = solutionString.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet())
+        
+        letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(letterBits) as! [String]
+        letterButtons = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(letterButtons) as! [UIButton]
+        
+        if letterBits.count == letterButtons.count {
+            for (i, letterBit) in letterBits.enumerate() {
+                letterButtons[i].setTitle(letterBit, forState: .Normal)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
