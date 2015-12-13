41b7172bd8ed057eb1ea0b0def1de4e5bc5ec761
Mike Stubna <mike@stubna.com>
2015-12-13 09:59:28 -0500

    project 12 initial checkin

---
+++ 12/Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10
+//
+//  Created by Mike Stubna on 12/12/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    
+}
