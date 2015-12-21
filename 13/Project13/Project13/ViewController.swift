b3d2f03cd640a4c2c9cb9f4a361a3fa89a6d8dca
Mike Stubna <mike@stubna.com>
2015-12-21 09:59:05 -0500

    project 13 initial checkin
    including swift linter

---
+++ 13/Project13/Project13/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Mike Stubna on 12/21/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
