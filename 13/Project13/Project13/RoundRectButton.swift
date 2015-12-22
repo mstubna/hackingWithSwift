1ea78c359f18627ebd8fc2f7dc5af18e9438099d
Mike Stubna <mike@stubna.com>
2015-12-22 09:15:33 -0500

    styling buttons

---
+++ 13/Project13/Project13/RoundRectButton.swift
@@ -0,0 +1,22 @@
+//
+//  RoundRectButton.swift
+//  Project13
+//
+//  Created by Mike Stubna on 12/22/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RoundRectButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.backgroundColor = UIColor.clearColor()
+        self.layer.cornerRadius = 5
+        self.layer.borderWidth = 1
+        self.layer.borderColor = self.tintColor.CGColor
+        self.titleEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10)
+    }
+}
