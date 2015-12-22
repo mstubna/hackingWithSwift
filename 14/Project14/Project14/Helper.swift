da7213fd8ccaf939cd7ca9ff3a5b05020ff3c563
Mike Stubna <mike@stubna.com>
2015-12-22 11:18:15 -0500

    finishing game

--- 14/Project14/Project14/Helper.swift
+++ 14/Project14/Project14/Helper.swift
@@ -34,7 +34,7 @@ func RandomColor() -> UIColor {
 	return UIColor(red: RandomCGFloat(), green: RandomCGFloat(), blue: RandomCGFloat(), alpha: 1)
 }
 
-func runAfterDelay(delay: NSTimeInterval, block: dispatch_block_t) {
+func RunAfterDelay(delay: NSTimeInterval, block: dispatch_block_t) {
 	let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
 	dispatch_after(time, dispatch_get_main_queue(), block)
 }
