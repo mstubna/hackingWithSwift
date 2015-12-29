fb71b4e32bc5148ae5b6ea8c701f59d71abc4536
Mike Stubna <mike@stubna.com>
2015-12-29 13:20:34 -0500

    Porject 16 initial checkin

---
+++ 16/Project16/Project16/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Mike Stubna on 12/29/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
