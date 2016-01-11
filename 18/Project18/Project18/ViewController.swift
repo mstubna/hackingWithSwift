c648e16e6918ccb7f5e5bbefa3541d9fb4eede1c
Mike Stubna <mike@stubna.com>
2016-01-11 08:20:19 -0500

    adding iAd to view

--- 18/Project18/Project18/ViewController.swift
+++ 18/Project18/Project18/ViewController.swift
@@ -6,17 +6,50 @@
 //  Copyright © 2016 Mike. All rights reserved.
 //
 
+import iAd
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
+
+    var bannerView: ADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        bannerView = ADBannerView(adType: .Banner)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+
+        let viewsDictionary = ["bannerView": bannerView]
+        view.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|[bannerView]|",
+                options: [],
+                metrics: nil,
+                views: viewsDictionary
+            )
+        )
+        view.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:[bannerView]|",
+                options: [],
+                metrics: nil,
+                views: viewsDictionary
+            )
+        )
+    }
+
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bannerView.hidden = false
+    }
+
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        bannerView.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
