da63f8e612a2a43e86cc98a81da10f4dd361bef8
Mike Stubna <mike@stubna.com>
2016-01-11 06:02:19 -0500

    Project 17 initial checkin

---
+++ 17/Project17/Project17/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Mike Stubna on 1/11/16.
+//  Copyright (c) 2016 Mike. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMoveToView(view: SKView) {
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
