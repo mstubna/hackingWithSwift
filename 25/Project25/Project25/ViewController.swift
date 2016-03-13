f7b0b1fff5d83d289a1e4526d0278f6c3a56979c
Mike Stubna <mike@stubna.com>
2016-03-13 16:09:56 -0400

    project 25 initial commit

---
+++ 25/Project25/Project25/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Mike Stubna on 3/13/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
