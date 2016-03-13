c1df6c1c4d66424b5eb4ccd2198a50916fe33211
Mike Stubna <mike@stubna.com>
2016-03-13 16:10:06 -0400

    project 24 playground

---
+++ 24/Project24.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Int {
+    mutating func plusOne() {
+        self += 1
+    }
+}
+
+var myInt = 0
+myInt.plusOne()
+myInt
