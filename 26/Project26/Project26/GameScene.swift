4b2c715c38efff296b09b6065a8d7bf18e7e89cd
Mike Stubna <mike@stubna.com>
2016-03-14 08:38:27 -0400

    code lint

--- 26/Project26/Project26/GameScene.swift
+++ 26/Project26/Project26/GameScene.swift
@@ -11,10 +11,10 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
     }
-    
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
     }
-   
+
     override func update(currentTime: CFTimeInterval) {
     }
 }
--- 26/Project26/Project26/GameViewController.swift
+++ 26/Project26/Project26/GameViewController.swift
@@ -16,16 +16,16 @@ class GameViewController: UIViewController {
 
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
-            let skView = self.view as! SKView
+            guard let skView = self.view as? SKView else { return }
             skView.showsFPS = true
             skView.showsNodeCount = true
-            
+
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-            
+
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
-            
+
             skView.presentScene(scene)
         }
     }
