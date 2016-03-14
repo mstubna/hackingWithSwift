0194ad9d897b3b91b585dde26333092138aedb0b
Mike Stubna <mike@stubna.com>
2016-03-14 08:36:21 -0400

    project 26 setup

---
+++ 26/Project26/Project26/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Mike Stubna on 3/14/16.
+//  Copyright (c) 2016 Mike. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+    }
+}
