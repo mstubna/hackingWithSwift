e7b8eecaea7afa914c9b1e410aeb725693ee4ce6
Mike Stubna <mike@stubna.com>
2016-03-14 15:52:38 -0400

    stubbing out boilerplate code

--- 27/Project27/Project27/ViewController.swift
+++ 27/Project27/Project27/ViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentDrawType = 0
+
+    @IBOutlet var imageView: UIImageView!
+
+    @IBAction func redrawTapped(sender: AnyObject) {
+        currentDrawType += 1
+
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+
+    func drawRectangle() {
+        print("hell0")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        drawRectangle()
     }
 
     override func didReceiveMemoryWarning() {
