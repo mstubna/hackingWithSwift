5390106b493f7d8b30ee164700ed03345e1b5218
Mike Stubna <mike@stubna.com>
2016-03-14 10:19:48 -0400

    Project 27 initial setup

---
+++ 27/Project27/Project27/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Mike Stubna on 3/14/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
