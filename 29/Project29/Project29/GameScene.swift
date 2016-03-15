daa301ea1c962ef0f48cb621f8c06cb01831b990
Mike Stubna <mike@stubna.com>
2016-03-15 14:05:49 -0400

    Project 29 initial setup

---
+++ 29/Project29/Project29/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Mike Stubna on 3/15/16.
+//  Copyright (c) 2016 Mike. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
