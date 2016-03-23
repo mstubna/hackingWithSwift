491dca100c6ee734d5f759ae1057fc3ca2068c53
Mike Stubna <mike@stubna.com>
2016-03-23 09:25:31 -0400

    Project 31 initial checkin

---
+++ 31/Project31/Project31/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Project31
+//
+//  Created by Mike Stubna on 3/23/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?
+    ) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+}
---
+++ 31/Project31/Project31/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Mike Stubna on 3/23/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var addressBar: UITextField!
+    @IBOutlet var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
