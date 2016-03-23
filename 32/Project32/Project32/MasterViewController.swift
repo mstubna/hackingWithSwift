f1638b5c2b65129975dc7511c5c8d0bb9f0456ae
Mike Stubna <mike@stubna.com>
2016-03-23 15:01:17 -0400

    adding dummy content for list view

--- 32/Project32/Project32/MasterViewController.swift
+++ 32/Project32/Project32/MasterViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var projects = [[String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        projects.append(
+            ["Project 1: Storm Viewer",
+            "Constants and variables, UITableView, UIImageView, NSFileManager, storyboards"]
+        )
+        projects.append(
+            ["Project 2: Guess the Flag",
+            "@2x and @3x images, asset catalogs, integers, doubles, floats, " +
+            "operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, " +
+            "actions, string interpolation, UIAlertController"]
+        )
+        projects.append(
+            ["Project 3: Social Media",
+            "UIBarButtonItem, UIActivityViewController, the Social framework, NSURL"]
+        )
+        projects.append(
+            ["Project 4: Easy Browser",
+            "loadView(), WKWebView, delegation, classes and structs, NSURLRequest, UIToolbar, " +
+            "UIProgressView., key-value observing"]
+        )
+        projects.append(
+            ["Project 5: Word Scramble",
+            "NSString, closures, method return values, booleans, NSRange"]
+        )
+        projects.append(
+            ["Project 6: Auto Layout",
+            "Get to grips with Auto Layout using practical examples and code"]
+        )
+        projects.append(
+            ["Project 7: Whitehouse Petitions",
+            "JSON, NSData, UITabBarController"]
+        )
+        projects.append(
+            ["Project 8: 7 Swifty Words",
+            "addTarget(), enumerate(), countElements(), find(), join(), property observers, " +
+            "range operators."]
+        )
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,7 +66,7 @@ class MasterViewController: UITableViewController {
     override func tableView(
         tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return projects.count
     }
 
     override func tableView(
@@ -37,6 +74,10 @@ class MasterViewController: UITableViewController {
         cellForRowAtIndexPath indexPath: NSIndexPath
     ) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let project = projects[indexPath.row]
+        cell.textLabel?.text = "\(project[0]): \(project[1])"
+
         return cell
     }
 
