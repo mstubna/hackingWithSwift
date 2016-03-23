0cdd4e39920e3d8c669e28c7b68d4f8de0bee2b5
Mike Stubna <mike@stubna.com>
2016-03-23 10:22:07 -0400

    adding app functionality

--- 31/Project31/Project31/ViewController.swift
+++ 31/Project31/Project31/ViewController.swift
@@ -8,13 +8,121 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate,
+    UIGestureRecognizerDelegate {
 
     @IBOutlet var addressBar: UITextField!
     @IBOutlet var stackView: UIStackView!
+    weak var activeWebView: UIWebView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setDefaultTitle()
+
+        let add = UIBarButtonItem(
+            barButtonSystemItem: .Add,
+            target: self,
+            action: #selector(ViewController.addWebView)
+        )
+        let delete = UIBarButtonItem(
+            barButtonSystemItem: .Trash,
+            target: self,
+            action: #selector(ViewController.deleteWebView)
+        )
+        navigationItem.rightBarButtonItems = [delete, add]
+    }
+
+    func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+
+    func addWebView() {
+        let webView = UIWebView()
+        webView.delegate = self
+
+        stackView.addArrangedSubview(webView)
+
+        let url = NSURL(string: "https://www.hackingwithswift.com")!
+        webView.loadRequest(NSURLRequest(URL: url))
+
+        webView.layer.borderColor = UIColor.blueColor().CGColor
+        selectWebView(webView)
+
+        let recognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(ViewController.webViewTapped(_:))
+        )
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+    }
+
+    func selectWebView(webView: UIWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let webView = activeWebView, address = addressBar.text {
+            if let url = NSURL(string: address) {
+                webView.loadRequest(NSURLRequest(URL: url))
+            }
+        }
+
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func webViewTapped(recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? UIWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+
+    func gestureRecognizer(
+        gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWithGestureRecognizer
+            otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
+    }
+
+    func deleteWebView() {
+        guard let webView = activeWebView else { return }
+        guard let index = stackView.arrangedSubviews.indexOf(webView) else { return }
+
+        // Remove webview from the stack view
+        stackView.removeArrangedSubview(webView)
+
+        // now remove it from the view hierarchy – this is important!
+        webView.removeFromSuperview()
+
+        if stackView.arrangedSubviews.count == 0 {
+            // go back to our default UI
+            setDefaultTitle()
+        } else {
+            // convert the Index value into an integer
+            var currentIndex = Int(index)
+
+            // if that was the last web view in the stack, go back one
+            if currentIndex == stackView.arrangedSubviews.count {
+                currentIndex = stackView.arrangedSubviews.count - 1
+            }
+
+            // find the web view at the new index and select it
+            if let newSelectedWebView = stackView.arrangedSubviews[currentIndex] as? UIWebView {
+                selectWebView(newSelectedWebView)
+            }
+        }
+    }
+
+    // Change the stack view orientation when app's size class changes
+    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        stackView.axis = traitCollection.horizontalSizeClass == .Compact ? .Vertical : .Horizontal
     }
 
     override func didReceiveMemoryWarning() {
