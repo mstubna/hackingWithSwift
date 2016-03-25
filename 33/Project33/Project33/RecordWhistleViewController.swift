8a5adefa9fa43473d9f3aae7bffe8fe334cebeb6
Mike Stubna <mike@stubna.com>
2016-03-25 08:30:37 -0400

    adding UIStackView

--- 33/Project33/Project33/RecordWhistleViewController.swift
+++ 33/Project33/Project33/RecordWhistleViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class RecordWhistleViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var stackView: UIStackView!
 
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        super.loadView()
+
+        view.backgroundColor = UIColor.grayColor()
+
+        stackView = UIStackView()
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackViewDistribution.FillEqually
+        stackView.alignment = UIStackViewAlignment.Center
+        stackView.axis = .Vertical
+        view.addSubview(stackView)
+
+        view.addConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|[stackView]|",
+                options: NSLayoutFormatOptions.AlignAllCenterX,
+                metrics: nil,
+                views: ["stackView": stackView]
+            )
+        )
+        view.addConstraint(
+            NSLayoutConstraint(
+                item: stackView,
+                attribute: .CenterY,
+                relatedBy: .Equal,
+                toItem: view,
+                attribute: .CenterY,
+                multiplier: 1,
+                constant:0
+            )
+        )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
