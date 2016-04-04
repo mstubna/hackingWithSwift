129bcee8c5595a96fb8151ff09493a2badbe2469
Mike Stubna <mike@stubna.com>
2016-04-04 11:04:30 -0400

    adding game presentation logic

--- 34/Project34/Project34/ViewController.swift
+++ 34/Project34/Project34/ViewController.swift
@@ -12,9 +12,68 @@ class ViewController: UIViewController {
 
     @IBOutlet var columnButtons: [UIButton]!
 
+    var placedChips = [[UIView]]()
+    var board: Board!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for _ in 0 ..< Board.width {
+            placedChips.append([UIView]())
+        }
+
+        resetBoard()
+    }
+
+    func resetBoard() {
+        board = Board()
+
+        for i in 0 ..< placedChips.count {
+            for chip in placedChips[i] {
+                chip.removeFromSuperview()
+            }
+
+            placedChips[i].removeAll(keepCapacity: true)
+        }
+    }
+
+    func addChipAtColumn(column: Int, row: Int, color: UIColor) {
+        let button = columnButtons[column]
+        let size = min(button.frame.size.width, button.frame.size.height / 6)
+        let rect = CGRect(x: 0, y: 0, width: size, height: size)
+
+        if placedChips[column].count < row + 1 {
+            let newChip = UIView()
+            newChip.frame = rect
+            newChip.userInteractionEnabled = false
+            newChip.backgroundColor = color
+            newChip.layer.cornerRadius = size / 2
+            newChip.center = positionForChipAtColumn(column, row: row)
+            newChip.transform = CGAffineTransformMakeTranslation(0, -800)
+            view.addSubview(newChip)
+
+            UIView.animateWithDuration(
+                0.5,
+                delay: 0,
+                options: .CurveEaseIn,
+                animations: { () -> Void in
+                    newChip.transform = CGAffineTransformIdentity
+                },
+                completion: nil
+            )
+
+            placedChips[column].append(newChip)
+        }
+    }
+
+    func positionForChipAtColumn(column: Int, row: Int) -> CGPoint {
+        let button = columnButtons[column]
+        let size = min(button.frame.size.width, button.frame.size.height / 6)
+
+        let xOffset = CGRectGetMidX(button.frame)
+        var yOffset = CGRectGetMaxY(button.frame) - size / 2
+        yOffset -= size * CGFloat(row)
+        return CGPoint(x: xOffset, y: yOffset)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +82,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func makeMove(sender: UIButton) {
-    }
+        let column = sender.tag
 
+        if let row = board.nextEmptySlotInColumn(column) {
+            board.addChip(.Red, column: column)
+            addChipAtColumn(column, row: row, color: .redColor())
+        }
+    }
 }
