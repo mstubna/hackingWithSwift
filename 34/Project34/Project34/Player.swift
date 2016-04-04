c40ecb14b2cbd248a315e2469fb57ff8e729208b
Mike Stubna <mike@stubna.com>
2016-04-04 11:11:18 -0400

    adding Player class

---
+++ 34/Project34/Project34/Player.swift
@@ -0,0 +1,47 @@
+//
+//  Player.swift
+//  Project34
+//
+//  Created by Mike Stubna on 4/4/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class Player: NSObject, GKGameModelPlayer {
+
+    var chipColor: ChipColor
+    var color: UIColor
+    var name: String
+    var playerId: Int
+
+    static var allPlayers = [
+        Player(chipColor: .Red),
+        Player(chipColor: .Black)
+    ]
+
+    // computed property that returns this player's opponent
+    var opponent: Player {
+        if chipColor == .Red {
+            return Player.allPlayers[1]
+        } else {
+            return Player.allPlayers[0]
+        }
+    }
+
+    init(chipColor: ChipColor) {
+        self.chipColor = chipColor
+        self.playerId = chipColor.rawValue
+
+        if chipColor == .Red {
+            color = .redColor()
+            name = "Red"
+        } else {
+            color = .blackColor()
+            name = "Black"
+        }
+
+        super.init()
+    }
+}
