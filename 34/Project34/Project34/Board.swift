016207ea29895d5e67e96fee6de4b7c487cfe9a6
Mike Stubna <mike@stubna.com>
2016-04-04 11:04:16 -0400

    adding Board model

---
+++ 34/Project34/Project34/Board.swift
@@ -0,0 +1,58 @@
+//
+//  Board.swift
+//  Project34
+//
+//  Created by Mike Stubna on 4/4/16.
+//  Copyright © 2016 Mike. All rights reserved.
+//
+
+import UIKit
+
+enum ChipColor: Int {
+    case None = 0
+    case Red
+    case Black
+}
+
+class Board: NSObject {
+
+    static var width = 7
+    static var height = 6
+    var slots = [ChipColor]()
+
+    override init() {
+        for _ in 0 ..< Board.width * Board.height {
+            slots.append(.None)
+        }
+
+        super.init()
+    }
+
+    func canMoveInColumn(column: Int) -> Bool {
+        return nextEmptySlotInColumn(column) != nil
+    }
+
+    func addChip(chip: ChipColor, column: Int) {
+        if let row = nextEmptySlotInColumn(column) {
+            setChip(chip, column: column, row: row)
+        }
+    }
+
+    func nextEmptySlotInColumn(column: Int) -> Int? {
+        for row in 0 ..< Board.height {
+            if chipInColumn(column, row: row) == .None {
+                return row
+            }
+        }
+
+        return nil
+    }
+
+    private func chipInColumn(column: Int, row: Int) -> ChipColor {
+        return slots[row + column * Board.height]
+    }
+
+    private func setChip(chip: ChipColor, column: NSInteger, row: NSInteger) {
+        slots[row + column * Board.height] = chip
+    }
+}
