9d52a7883910cd04776c95a4060e9f6418479a94
Mike Stubna <mike@stubna.com>
2016-04-05 22:29:46 -0400

    commenting debug statements

--- 36/Project36/Project36/GameViewController.swift
+++ 36/Project36/Project36/GameViewController.swift
@@ -17,9 +17,9 @@ class GameViewController: UIViewController {
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             guard let skView = self.view as? SKView else { return }
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            skView.showsPhysics = true
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
+//            skView.showsPhysics = true
 
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
